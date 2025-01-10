34cca52d1aa87e367ac6587c010a216046cf30bf
Rockybhaiji07 <86160003+Rockybhaiji07@users.noreply.github.com>
2025-01-10 10:56:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @herokutech
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@herokutech
+# Ask Doubt on telegram @OwnerRocky
 
 from os import environ 
 
@@ -13,9 +13,9 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @herokutech
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@herokutech
+# Ask Doubt on telegram @OwnerRocky
 
 class temp(object): 
     lock = {}
@@ -24,6 +24,6 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @herokutech
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@herokutech
+# Ask Doubt on telegram @OwnerRocky
